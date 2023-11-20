cdc985615f06a2fbba831861a7f1554fdcb0ff52
nooblette <min9hyuk@gmail.com>
2023-11-20 20:25:04 +0900

    feat : ch10 - 라이브러리와 모듈(10.6. 전이 의존)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module thisIsJava.module.a {
 	exports package1;
 	exports package2;
+
+	requires transitive thisIsJava.module.b;
 }
--- src/main/java/package1/A.java
+++ src/main/java/package1/A.java
@@ -1,8 +1,14 @@
 package package1;
 
+import package3.C;
 public class A {
 	// 메서드 선언
 	public void method(){
 		System.out.println("thisIsJava_module_a package1 A class method 실행");
+	}
+
+	public C getC() {
+		C c = new C();
+		return c;
 	}
 }
